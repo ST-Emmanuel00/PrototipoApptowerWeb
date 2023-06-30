6692b32dc6cc7e427211e98be5e9f2089a02384d
DAYORITO <107157823+DAYORITO@users.noreply.github.com>
2023-06-30 09:46:01 -0500

    Add files via upload

---
+++ public/js/multaCrear.js
@@ -0,0 +1,130 @@
+// console.log("Hola")
+
+document.addEventListener('DOMContentLoaded', function(){
+let fechaL = document.getElementById('fechaL')
+let fechaActual = document.querySelector('#fechaC');
+let botonGenerar= document.querySelector('#botonGenerar');
+let file = document.querySelector('#customFile');
+const datos=[
+    {apartamento: 404,
+        residente: "Paula Gomez"},
+        {
+            apartamento: 505,
+            residente: "Jonathan Gimenez"
+        }
+]
+
+const datosM = [
+    { 
+        motivo: "Mal uso de estacionamiento",
+        descripcion: "Se reportarion varias incidencias en parqueo por mas de 3 dias en estacionamiento de visitantes"   
+    },
+
+    {
+        motivo: "Aqui otro motivo",
+        descripcion: "Se crea otra descripcion"
+    }
+]
+
+// Función para actualizar el campo de entrada del residente
+function actualizarResidente() {
+    const apartamentoSelect = document.getElementById("apartamento");
+    const residenteInput = document.getElementById("residente");
+    
+    // Obtener el apartamento seleccionado
+    const apartamentoSeleccionado = apartamentoSelect.value;
+    
+    // Buscar el objeto de datos correspondiente al apartamento seleccionado
+    const seleccionado = datos.find(dato => dato.apartamento == apartamentoSeleccionado);
+    
+    // Actualizar el valor del campo de entrada del residente
+    if (seleccionado) {
+      residenteInput.value = seleccionado.residente;
+    } else {
+      residenteInput.value = "";
+    }
+  }
+
+  // Asignar el evento onchange al select de apartamento desde el código JavaScript
+  const apartamentoSelect = document.getElementById("apartamento");
+  apartamentoSelect.addEventListener("change", actualizarResidente);
+
+  // Inicializar el select con las opciones del array de datos
+  datos.forEach(dato => {
+    const option = document.createElement("option");
+    option.value = dato.apartamento;
+    option.text = dato.apartamento;
+    apartamentoSelect.appendChild(option);
+  });
+  
+  function actualizarDescripcion() {
+    const motivoSelect = document.getElementById("motivo");
+    const descripcionTextarea = document.getElementById("descripcion");
+    
+    // Obtener el motivo seleccionado
+    const motivoSeleccionado = motivoSelect.value;
+    
+    // Buscar el objeto de datos correspondiente al motivo seleccionado
+    const seleccionado = datosM.find(dato => dato.motivo === motivoSeleccionado);
+    
+    // Actualizar el valor del campo de texto de la descripción
+    if (seleccionado) {
+      descripcionTextarea.value = seleccionado.descripcion;
+    } else {
+      descripcionTextarea.value = "";
+    }
+  }
+
+  // Asignar el evento onchange al select del motivo desde el código JavaScript
+  const motivoSelect = document.getElementById("motivo");
+  motivoSelect.addEventListener("change", actualizarDescripcion);
+
+  // Inicializar el select con las opciones del array de datos
+  datosM.forEach(dato => {
+    const option = document.createElement("option");
+    option.value = dato.motivo;
+    option.text = dato.motivo;
+    motivoSelect.appendChild(option);
+  });
+
+  fechaActual.value='2023-05-05'
+  console.log(fechaActual)
+
+  if(fechaL){
+  fechaL.addEventListener('blur', function(){
+    console.log("buneas")
+    var fechaLValue = fechaL.value;
+      var fechaLimite = new Date(fechaLValue);
+      var fechaActual = new Date();
+      var fechaValida = fechaLimite > fechaActual;
+
+      if (fechaLValue === "" || !fechaValida) {
+        fechaL.classList.remove("border-success");
+        fechaL.classList.add("border-danger");
+      } else {
+        fechaL.classList.remove("border-danger");
+        fechaL.classList.add("border-success");
+      }
+  });
+}
+
+botonGenerar.addEventListener('click', function(){
+    try{
+    if(fechaL == null || fechaL.classList.contains("border-danger") || file.length == 0){
+        throw new Error("No se aceptan el campos vacios")
+    }
+    Swal.fire(
+        'Exito',
+        'Se ha registrado la multa',
+        'success'
+    )
+}catch(e){
+    Swal.fire(
+        'Error',
+        e.message,
+        'error'
+    )
+}
+});
+
+})
--- public/js/multaEditar.js
+++ public/js/multaEditar.js
@@ -1,44 +1,65 @@
-document.addEventListener('DOMContentLoaded', function() {
-    console.log('holi')
-    console.log(confirmacion())
-    var fechaLE = document.getElementById('fechaLE');
-    let aprobarbtn = document.getElementById('aprobar');
-  
+document.addEventListener("DOMContentLoaded", function () {
+  console.log("holi");
+  var fechaLE = document.getElementById("fechaLE");
+  let aprobarbtn = document.getElementById("aprobar");
+  let botoncito = document.getElementById("botoncito");
 
-    fechaLE.addEventListener('onblur', function() {
+  if (fechaLE) {
+    fechaLE.addEventListener("blur", function () {
+      console.log("ingresa")
       var fechaLEValue = fechaLE.value;
       var fechaLimite = new Date(fechaLEValue);
       var fechaActual = new Date();
       var fechaValida = fechaLimite > fechaActual;
-  
-      if (fechaLEValue === '' || !fechaValida) {
-        fechaLE.classList.remove('border-success');
-        fechaLE.classList.add('border-danger');
+
+      if (fechaLEValue === "" || !fechaValida) {
+        fechaLE.classList.remove("border-success");
+        fechaLE.classList.add("border-danger");
       } else {
-        fechaLE.classList.remove('border-danger');
-        fechaLE.classList.add('border-success');
+        fechaLE.classList.remove("border-danger");
+        fechaLE.classList.add("border-success");
       }
     });
+  }
+  botoncito.addEventListener("click", function(){
+    try{
+      
+      if(fechaLE.classList.contains('border-danger')||fechaLE==""){
+        throw new Error("Fecha no valida o campo vacio")
+      }
+      Swal.fire(
+        'Proceso exitoso',
+        'Cambios guardados',
+        'success'
 
-    aprobarbtn.addEventListener('click', function(e){
-        e.preventDefault();
-        Swal.fire({
-            title: 'Aprobar',
-            text: "¿Estas seguro de aprobar el registro?",
-            icon: 'question',
-            showCancelButton: true,
-            confirmButtonColor: '#3085d6',
-            cancelButtonColor: '#d33',
-            confirmButtonText: 'Si'
-          }).then((result) => {
-            if (result.isConfirmed) {
-              Swal.fire(
-                'Aprobado',
-                'El pago de la multa ha sido verificado con exito',
-                'success'
-              )
-            }
-          })
-    })
+      )
+    }catch(e){
+      Swal.fire(
+        'Error',
+        e.message,
+        'error'
+      )
+    }
+  })
+    
+
+  aprobarbtn.addEventListener("click", function (e) {
+    Swal.fire({
+      title: "Aprobar",
+      text: "¿Estas seguro de aprobar el registro?",
+      icon: "question",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Si",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        Swal.fire(
+          "Aprobado",
+          "El pago de la multa ha sido verificado con exito",
+          "success"
+        );
+      }
+    });
   });
-  
+});
